a04c4976b50d13096dede6eaf9af5e6ae2ac6731
Will <wpgrimmer@loyola.edu>
2021-09-22 21:58:23 -0400

    finished

--- primerAndPaint.py
+++ primerAndPaint.py
@@ -9,7 +9,7 @@ l=float(input("Please give the length in feet"))
 w=float(input("Please give the width in feet"))
 h=float(input("Please give the height in feet"))
 
-area = l*w*2 +l*h*2 +w*h*2
+area = l*w +l*h*2 +w*h*2
 primer = area/200
 paint = area/350
 
